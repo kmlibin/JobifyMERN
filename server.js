d96274605d2a1c39c04c5a7ce0968400c994ba2f
Kelli Libin <km.putnam2@gmail.com>
2024-11-25 14:28:34 -0700

    created and implemented routes/controllers

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ dotenv.config();
 
 import express from "express";
 const app = express();
-import { nanoid } from "nanoid";
 import morgan from "morgan";
+
+//routers
+import jobRouter from "./routes/jobRoutes.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-let jobs = [
-  { id: nanoid(), company: "apple", position: "frontend" },
-  { id: nanoid(), company: "company2", position: "backend" },
-];
 
 //how you get access to the req object
 app.use(express.json());
@@ -21,59 +18,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("...home page");
 });
-//get all jobs
-app.get("/api/v1/jobs", (req, res) => {
-  res.status(200).json({ jobs });
+
+app.use("/api/v1/jobs", jobRouter);
+
+//if a route/page isn't found. needs to run after all routes, because order matters for routes (recall)
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
 });
 
-//create job
-app.post("/api/v1/jobs", (req, res) => {
-  //controller will create id in future
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ message: "please provide company and position" });
-  }
-  //since not connected to db, pushing manually to array
-  const id = nanoid(10);
-  const job = { id, company, position };
-  //since not connected to db, pushing manually to array
-  jobs.push(job);
-
-  res.status(201).json({ job });
-});
-
-//get one job
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res
-      .status(404)
-      .json({ message: `Job not found with that id ${id}` });
-  }
-
-  res.status(200).json({ job });
-});
-
-//editing job
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ message: "please provide job and company" });
-  }
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res
-      .status(404)
-      .json({ message: `Job not found with that id ${id}` });
-  }
-  job.company = company;
-  job.position = position;
-
-  res.status(200).json({ message: "job modified", job });
+//error middleware. HAS to be the last one
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: "something went wrong" });
 });
 
 //set up port variable
@@ -82,3 +37,106 @@ const port = process.env.PORT || 4100;
 app.listen(port, () => {
   console.log("server running on port 5100");
 });
+
+//sample simple server using local data
+// import * as dotenv from "dotenv";
+// dotenv.config();
+
+// import express from "express";
+// const app = express();
+// import { nanoid } from "nanoid";
+// import morgan from "morgan";
+
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
+
+// let jobs = [
+//   { id: nanoid(), company: "apple", position: "frontend" },
+//   { id: nanoid(), company: "company2", position: "backend" },
+// ];
+
+// //how you get access to the req object
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("...home page");
+// });
+// //get all jobs
+// app.get("/api/v1/jobs", (req, res) => {
+//   res.status(200).json({ jobs });
+// });
+
+// //create job
+// app.post("/api/v1/jobs", (req, res) => {
+//   //controller will create id in future
+//   const { company, position } = req.body;
+//   if (!company || !position) {
+//     return res
+//       .status(400)
+//       .json({ message: "please provide company and position" });
+//   }
+//   //since not connected to db, pushing manually to array
+//   const id = nanoid(10);
+//   const job = { id, company, position };
+//   //since not connected to db, pushing manually to array
+//   jobs.push(job);
+
+//   res.status(201).json({ job });
+// });
+
+// //get one job
+// app.get("/api/v1/jobs/:id", (req, res) => {
+//   const { id } = req.params;
+//   const job = jobs.find((job) => job.id === id);
+//   if (!job) {
+//     return res
+//       .status(404)
+//       .json({ message: `Job not found with that id ${id}` });
+//   }
+
+//   res.status(200).json({ job });
+// });
+
+// //editing job
+// app.patch("/api/v1/jobs/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { company, position } = req.body;
+//   if (!company || !position) {
+//     return res.status(400).json({ message: "please provide job and company" });
+//   }
+//   const job = jobs.find((job) => job.id === id);
+//   if (!job) {
+//     return res
+//       .status(404)
+//       .json({ message: `Job not found with that id ${id}` });
+//   }
+//   job.company = company;
+//   job.position = position;
+
+//   res.status(200).json({ message: "job modified", job });
+// });
+
+// //delete job
+// app.delete("/api/v1/jobs/:id", (req, res) => {
+//   const { id } = req.params;
+
+//   const job = jobs.find((job) => job.id === id);
+//   if (!job) {
+//     return res
+//       .status(404)
+//       .json({ message: `Job not found with that id ${id}` });
+//   }
+
+//   const newJobs = jobs.filter((job) => job.id !== id);
+//   jobs = newJobs;
+
+//   res.status(200).json({ message: "job has been deleted" });
+// });
+
+// //set up port variable
+// const port = process.env.PORT || 4100;
+
+// app.listen(port, () => {
+//   console.log("server running on port 5100");
+// });
