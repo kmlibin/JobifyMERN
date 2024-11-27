688bbba39aeafdbda554d9fe3869cd2cf94ee919
Kelli Libin <km.putnam2@gmail.com>
2024-11-27 15:12:01 -0700

    add logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
-import { validateRegisterInput,validateLoginInput } from "../middleware/validationMiddleware.js";
+import {
+  loginUser,
+  logout,
+  registerUser,
+} from "../controllers/userController.js";
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
 router.post("/register", validateRegisterInput, registerUser);
 router.post("/login", validateLoginInput, loginUser);
+router.get("/logout", logout);
 
 export default router;
