af6caffa297d5fa7a079f8176b09cd7ebd4da2bc
Kelli Libin <km.putnam2@gmail.com>
2024-12-11 15:25:09 -0700

    added loader to dashboard layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import {
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
+import {loader as dashboardLoader} from './pages/DashboardLayout'
 
 //checks and applies for landing,login,error pages. in dashboard we will call the function separately
 export const checkDefaultTheme = () => {
@@ -50,6 +51,7 @@ const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <DashboardLayout />,
+        loader: dashboardLoader,
         children: [
           { index: true, element: <AddJob /> },
           { path: "stats", element: <Stats /> },
