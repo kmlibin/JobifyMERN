7182c528f7a79d0b70fb69514316bdfe063cd403
Kelli Libin <km.putnam2@gmail.com>
2024-12-11 14:46:10 -0700

    apply form Action to login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormRow } from "../components";
-import { Link } from "react-router-dom";
+import { Link, Form, redirect, useNavigation } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
+//register page has notes on this
+export const action = async ({ request }) => {
+  console.log("runs");
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login Successful");
+    return redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.message);
+    return error;
+  }
+};
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
-      <form className="form">
+      <Form className="form" method="post">
         <Logo />
         <h4>Log In</h4>
         <FormRow
@@ -20,15 +40,19 @@ const Login = () => {
           defaultValue="Secret890"
           labelText="Password"
         />
-        <button type="submit" className="btn btn-block">Submit</button>
-        <button type="button" className="btn btn-block">Explore the App</button>
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
+        <button type="button" className="btn btn-block">
+          Explore the App
+        </button>
         <p>
           Not a member yet?
           <Link to="/register" className="member-btn">
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
