2b4a1c078b064ed1105a3cb99c95f1f16aaf1567
Kelli Libin <km.putnam2@gmail.com>
2024-12-12 16:19:31 -0700

    remove data from curlies

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -7,7 +7,7 @@ import { useContext, createContext } from "react";
 //fetch data before component loads, grab it and use it in the useLoaderData hook
 export const loader = async ({ request }) => {
   try {
-    const { data } = await customFetch.get("/jobs");
+    const  data = await customFetch.get("/jobs");
     console.log(data);
     return data;
   } catch (error) {
