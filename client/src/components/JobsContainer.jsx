145777dff0c1a561183126123206ea61b1d1f834
Kelli Libin <km.putnam2@gmail.com>
2024-12-12 16:19:42 -0700

    set up components to display each job

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Job from "../components/Job";
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { useAllJobsContext } from "../pages/AllJobs";
 
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No Jobs to Display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
