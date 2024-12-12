a54ed5dbc980406b1ebef76d778a239910a5baaa
Kelli Libin <km.putnam2@gmail.com>
2024-12-12 16:56:13 -0700

    added edit and delete funcitonality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,16 @@ import {
   AllJobs,
   Profile,
   Admin,
+  EditJob,
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { action as addJobAction } from "./pages/AddJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
+import { action as editJobAction } from "./pages/EditJob";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
 
 //checks and applies for landing,login,error pages. in dashboard we will call the function separately
 export const checkDefaultTheme = () => {
@@ -60,6 +64,13 @@ const router = createBrowserRouter([
           { path: "all-jobs", element: <AllJobs />, loader: allJobsLoader },
           { path: "profile", element: <Profile /> },
           { path: "admin", element: <Admin /> },
+          { path: "delete-job/:id", action: deleteJobAction },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            action: editJobAction,
+            loader: editJobLoader,
+          },
         ],
       },
     ],
--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -1,10 +1,10 @@
-import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa';
-import { Link, Form } from 'react-router-dom';
-import Wrapper from '../assets/wrappers/Job';
-import JobInfo from './JobInfo';
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link, Form } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Job";
+import JobInfo from "./JobInfo";
 
-import day from 'dayjs';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
+import day from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
 day.extend(advancedFormat);
 
 const Job = ({
@@ -16,30 +16,34 @@ const Job = ({
   createdAt,
   jobStatus,
 }) => {
-  const date = day(createdAt).format('MMM Do, YYYY');
+  const date = day(createdAt).format("MMM Do, YYYY");
 
   return (
     <Wrapper>
       <header>
         {/* icon container */}
-        <div className='main-icon'>{company.charAt(0)}</div>
-        <div className='info'>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
           <h5>{position}</h5>
           <p>{company}</p>
         </div>
       </header>
-      <div className='content'>
-        <div className='content-center'>
+      <div className="content">
+        <div className="content-center">
           <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
           <JobInfo icon={<FaCalendarAlt />} text={date} />
           <JobInfo icon={<FaBriefcase />} text={jobType} />
           <div className={`status ${jobStatus}`}>{jobStatus}</div>
         </div>
 
-        <footer className='actions'>
-          <Link className='btn edit-btn'>Edit</Link>
-          <Form>
-            <button type='submit' className='btn delete-btn'>
+        <footer className="actions">
+          {/* //can do as relative path */}
+          {/* <Link to= {`../edit-job`} className='btn edit-btn'>Edit</Link> */}
+          <Link to={`/dashboard/edit-job/${_id}`} className="btn edit-btn">
+            Edit
+          </Link>
+          <Form method="post" action={`../delete-job/${_id}`}>
+            <button type="submit" className="btn delete-btn">
               Delete
             </button>
           </Form>
