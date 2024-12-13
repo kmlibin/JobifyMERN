ffae33eb0c8fcd3ac37c66d2235f2e14c594eab9
Kelli Libin <km.putnam2@gmail.com>
2024-12-13 15:50:59 -0700

    display avatar

--- client/src/components/LogoutContainer.jsx
+++ client/src/components/LogoutContainer.jsx
@@ -12,12 +12,21 @@ const LogoutContainer = () => {
         className="btn logout-btn"
         onClick={() => setShowLogout(!showLogout)}
         type="button"
-      ><FaUserCircle/>
-      {user?.name}
-      <FaCaretDown />
+      >
+        {/* //user only has avatar if they set it up - it's not input at registration. */}
+        {user.avatar ? (
+          <img src={user.avatar} alt="avatar" className="img" />
+        ) : (
+          <FaUserCircle />
+        )}
+
+        {user?.name}
+        <FaCaretDown />
       </button>
-      <div className={showLogout? 'dropdown show-dropdown' : 'dropdown'}>
-        <button type="button" className="dropdown-btn" onClick={logoutUser}>Logout</button>
+      <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+        <button type="button" className="dropdown-btn" onClick={logoutUser}>
+          Logout
+        </button>
       </div>
     </Wrapper>
   );
